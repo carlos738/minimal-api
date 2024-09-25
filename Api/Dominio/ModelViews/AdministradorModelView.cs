9a93a8affa7451d894433244937ae304c42d3470
carlos738 <carlosasl135@hotmail.com>
2024-09-25 14:38:39 -0300

    inicio

--- Api/Dominio/ModelViews/AdministradorModelView.cs
+++ Api/Dominio/ModelViews/AdministradorModelView.cs
@@ -7,4 +7,5 @@ public record AdministradorModelView
     public int Id { get;set; } = default!;
     public string Email { get;set; } = default!;
     public string Perfil { get;set; } = default!;
+    public string Endereco {get;set;} = default!;
 }
